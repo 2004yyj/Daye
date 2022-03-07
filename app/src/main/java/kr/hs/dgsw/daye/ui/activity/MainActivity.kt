2eef7c3870281552e3858ba2c13b4c51f8880798
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-07 15:59:00 +0900

    [ADD] 메인화면 프래그먼트 추가

--- app/src/main/java/kr/hs/dgsw/daye/ui/activity/MainActivity.kt
+++ app/src/main/java/kr/hs/dgsw/daye/ui/activity/MainActivity.kt
@@ -2,9 +2,28 @@ package kr.hs.dgsw.daye.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
+import kr.hs.dgsw.daye.databinding.ActivityMainBinding
+import kr.hs.dgsw.daye.ui.util.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        navController = findNavController(binding.navHostFragmentMain.getFragment())
+        binding.bnvMain.setupWithNavController(navController)
     }
 }

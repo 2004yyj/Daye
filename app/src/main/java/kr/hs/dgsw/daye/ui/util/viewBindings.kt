7400c493d8b0d186b61766685607cc69f6dfe569
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-07 20:25:19 +0900

    [ADD] HomeFragment 내부에 들어갈 Fragment 추가

--- app/src/main/java/kr/hs/dgsw/daye/ui/util/viewBindings.kt
+++ app/src/main/java/kr/hs/dgsw/daye/ui/util/viewBindings.kt
@@ -2,6 +2,7 @@ package kr.hs.dgsw.daye.ui.util
 
 import android.app.Activity
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
 inline fun <reified VB: ViewBinding> Activity.viewBindings(
@@ -10,4 +11,12 @@ inline fun <reified VB: ViewBinding> Activity.viewBindings(
     return lazy(LazyThreadSafetyMode.NONE) {
         inflate(layoutInflater)
     }
-} 
+}
+
+inline fun <reified VB: ViewBinding> Fragment.viewBindings(
+    crossinline inflate: (LayoutInflater) -> VB
+): Lazy<VB> {
+    return lazy(LazyThreadSafetyMode.NONE) {
+        inflate(layoutInflater)
+    }
+}

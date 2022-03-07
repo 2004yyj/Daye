7d933e03b7ede8f26a317d8be6b9e068e02ba1d7
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-07 16:02:13 +0900

    [ADD] viewBindings lazy 함수 추가

--- app/src/main/java/kr/hs/dgsw/daye/ui/activity/MainActivity.kt
+++ app/src/main/java/kr/hs/dgsw/daye/ui/activity/MainActivity.kt
@@ -8,15 +8,15 @@ import androidx.navigation.NavController
 import androidx.navigation.ui.setupWithNavController
 import kr.hs.dgsw.daye.databinding.ActivityMainBinding
 import kr.hs.dgsw.daye.ui.util.findNavController
+import kr.hs.dgsw.daye.ui.util.viewBindings
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by viewBindings(ActivityMainBinding::inflate)
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
